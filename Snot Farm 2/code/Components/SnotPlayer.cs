db79d0ded70e2f202faa956271d722e19cf707a2
yuberee <59583743+yuberee@users.noreply.github.com>
2024-02-03 12:33:41 +0100

    Episode 2 :)

--- Snot Farm 2/code/Components/SnotPlayer.cs
+++ Snot Farm 2/code/Components/SnotPlayer.cs
@@ -3,18 +3,22 @@ using Sandbox.Citizen;
 public sealed class SnotPlayer : Component
 {
 	[Property]
+	[Category( "Components" )]
 	public GameObject Camera { get; set; }
 
 	[Property]
+	[Category( "Components" )]
 	public CharacterController Controller { get; set; }
 
 	[Property]
+	[Category( "Components" )]
 	public CitizenAnimationHelper Animator { get; set; }
 
 	/// <summary>
 	/// How fast you can walk (Units per second)
 	/// </summary>
 	[Property]
+	[Category( "Stats" )]
 	[Range( 0f, 400f, 1f )]
 	public float WalkSpeed { get; set; } = 120f;
 
@@ -22,6 +26,7 @@ public sealed class SnotPlayer : Component
 	/// How fast you can run (Units per second)
 	/// </summary>
 	[Property]
+	[Category( "Stats" )]
 	[Range( 0f, 800f, 1f )]
 	public float RunSpeed { get; set; } = 250f;
 
@@ -29,23 +34,78 @@ public sealed class SnotPlayer : Component
 	/// How powerful you can jump (Units per second)
 	/// </summary>
 	[Property]
+	[Category( "Stats" )]
 	[Range( 0f, 1000f, 10f )]
 	public float JumpStrength { get; set; } = 400f;
+
+	/// <summary>
+	/// Where the camera rotates around and the aim originates from
+	/// </summary>
+	[Property]
+	public Vector3 EyePosition { get; set; }
+
+	public Angles EyeAngles { get; set; }
+	Transform _initialCameraTransform;
+
+	protected override void DrawGizmos()
+	{
+		Gizmo.Draw.LineSphere( EyePosition, 10f );
+	}
 
 
 	protected override void OnUpdate()
 	{
+		EyeAngles += Input.AnalogLook;
+		EyeAngles = EyeAngles.WithPitch( MathX.Clamp( EyeAngles.pitch, -80f, 80f ) );
+		Transform.Rotation = Rotation.FromYaw( EyeAngles.yaw );
 
+		if ( Camera != null )
+			Camera.Transform.Local = _initialCameraTransform.RotateAround( EyePosition, EyeAngles.WithYaw( 0f ) );
 	}
 
 	protected override void OnFixedUpdate()
 	{
 		base.OnFixedUpdate();
+
+		if ( Controller == null ) return;
+
+		var wishSpeed = Input.Down( "Run" ) ? RunSpeed : WalkSpeed;
+		var wishVelocity = Input.AnalogMove.Normal * wishSpeed * Transform.Rotation;
+
+		Controller.Accelerate( wishVelocity );
+
+		if ( Controller.IsOnGround )
+		{
+			Controller.Acceleration = 10f;
+			Controller.ApplyFriction( 5f );
+
+			if ( Input.Pressed( "Jump" ) )
+			{
+				Controller.Acceleration = 5f;
+				Controller.Punch( Vector3.Up * JumpStrength );
+
+				if ( Animator != null )
+					Animator.TriggerJump();
+			}
+		}
+		else
+		{
+			Controller.Velocity += Scene.PhysicsWorld.Gravity * Time.Delta;
+		}
+
+		Controller.Move();
+
+		if ( Animator != null )
+		{
+			Animator.IsGrounded = Controller.IsOnGround;
+			Animator.WithVelocity( Controller.Velocity );
+		}
 	}
 
 	protected override void OnStart()
 	{
-		base.OnStart();
+		if ( Camera != null )
+			_initialCameraTransform = Camera.Transform.Local;
 	}
 
 	protected override void OnEnabled()
